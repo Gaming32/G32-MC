2efcd7e90faf3e8f786aba1c9ba98d38c1d7bfbd
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2019-07-30 17:24:27 -0500

    Build 5

--- Assets/Scripts/CubeBase.cs
+++ Assets/Scripts/CubeBase.cs
@@ -21,10 +21,24 @@ public class CubeBase : MonoBehaviour
 
         topMaterial = new Material(Shader.Find("Standard"));
         topMaterial.mainTexture = topTexture;
+        topMaterial.SetFloat("_Metallic", 0f);
+        topMaterial.SetFloat("_Glossiness", 0f);
+        topMaterial.SetFloat("_SpecularHighlights", 0f);
+        topMaterial.SetFloat("_GlossyReflections", 0f);
+
         bottomMaterial = new Material(Shader.Find("Standard"));
         bottomMaterial.mainTexture = bottomTexture;
+        bottomMaterial.SetFloat("_Metallic", 0f);
+        bottomMaterial.SetFloat("_Glossiness", 0f);
+        bottomMaterial.SetFloat("_SpecularHighlights", 0f);
+        bottomMaterial.SetFloat("_GlossyReflections", 0f);
+
         sideMaterial = new Material(Shader.Find("Standard"));
         sideMaterial.mainTexture = sideTexture;
+        sideMaterial.SetFloat("_Metallic", 0f);
+        sideMaterial.SetFloat("_Glossiness", 0f);
+        sideMaterial.SetFloat("_SpecularHighlights", 0f);
+        sideMaterial.SetFloat("_GlossyReflections", 0f);
 
         topObj.GetComponent<MeshRenderer>().material = topMaterial;
         bottomObj.GetComponent<MeshRenderer>().material = bottomMaterial;
